b9b2a6b74f1a090e855a0a5416a33f0ce51c9807
Alexandra Roth <alroth8@gmail.com>
2017-04-15 17:08:21 -0400

    migrated

--- db/migrate/20170415204701_create_programs.rb
+++ db/migrate/20170415204701_create_programs.rb
@@ -1,7 +1,8 @@
 class CreatePrograms < ActiveRecord::Migration[5.0]
   def change
     create_table :programs do |t|
-
+    	t.references :workout
+    	t.references :trainee
       t.timestamps
     end
   end
